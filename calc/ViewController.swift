33fb06724a5c209f4824a9ad03e4a9cfa7bfdd93
Nikita <97m.nik@gmail.com>
2021-05-04 16:43:10 +0300

    add Uitest and minor fix

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -13,179 +13,111 @@ class ViewController: UIViewController {
 	var firstNumber: Double = 0
 	var secondNumber: Double = 0
 	var operation: String = ""
-	var check = true
+	var checkFirstnumAndSecondnum = true
 	var checkPoint = false
 	var ten = 10.0
 	
-	func forTrailingZero(temp: Double) -> String {
-		let tempVar = String(format: "%g", temp)
-		return tempVar
-	}
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		initButtom()
+		initLabel()
+	}
+	
+	// MARK:- Инициализация интрефейса
+	private func  initButtom(){
+		button(number: "/", valueLeft: 295, valueTop: 210)
+		button(number: "X", valueLeft: 295, valueTop: 305)
+		button(number: "-", valueLeft: 295, valueTop: 400)
+		button(number: "+", valueLeft: 200, valueTop: 495)
 		
-		inilizator()
-		buttomReset()
-		buttomEqually()
+		button(number: "=", valueLeft: 295, valueTop: 495)
+		button(number: ".", valueLeft: 105, valueTop: 495)
+		button(number: "C", valueLeft: 295, valueTop: 595)
 		
-		buttomOperation(valueOpertion: "/", valueLeft: 295, valueTop: 210)
-		buttomOperation(valueOpertion: "X", valueLeft: 295, valueTop: 305)
-		buttomOperation(valueOpertion: "-", valueLeft: 295, valueTop: 400)
-		buttomOperation(valueOpertion: "+", valueLeft: 200, valueTop: 495)
-		
-		button(number: "7", valueLeft: 10, valueTop: 210)
-		button(number: "8", valueLeft: 105, valueTop: 210)
-		button(number: "9", valueLeft: 200, valueTop: 210)
-		
-		button(number: "4", valueLeft: 10, valueTop: 305)
-		button(number: "5", valueLeft: 105, valueTop: 305)
-		button(number: "6", valueLeft: 200, valueTop: 305)
-		
+		button(number: "0", valueLeft: 10, valueTop: 495)
 		button(number: "1", valueLeft: 10, valueTop: 400)
 		button(number: "2", valueLeft: 105, valueTop: 400)
 		button(number: "3", valueLeft: 200, valueTop: 400)
-		
-		button(number: "0", valueLeft: 10, valueTop: 495)
-		
-		buttomPoint()
-		//button(number: ".", valueLeft: 105, valueTop: 495)
+		button(number: "4", valueLeft: 10, valueTop: 305)
+		button(number: "5", valueLeft: 105, valueTop: 305)
+		button(number: "6", valueLeft: 200, valueTop: 305)
+		button(number: "7", valueLeft: 10, valueTop: 210)
+		button(number: "8", valueLeft: 105, valueTop: 210)
+		button(number: "9", valueLeft: 200, valueTop: 210)
 	}
 	
-	
-	
-	private func inilizator() {
+	private func initLabel() {
 		label.text = "0"
 		label.backgroundColor = UIColor.blue
 		label.font = UIFont.systemFont(ofSize: 40)
+		self.label.accessibilityIdentifier = "answerView"
 		self.view.addSubview(label)
-		
 		label.snp.makeConstraints { (make) in
 			make.height.equalTo(150)
 			make.width.equalTo(375)
 			make.top.equalTo(50)
 			make.left.equalToSuperview().offset(10)
-			//make.right.equalToSuperview().offset(10)
 		}
 	}
 	
 	func button(number:String, valueLeft: Int, valueTop:Int){
-		
 		let button = UIButton()
-		//let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
 		button.setTitle(number, for: .normal)
 		button.backgroundColor = UIColor.orange
+		button.accessibilityIdentifier = String("identifier" + number)
 		button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
 		self.view.addSubview(button)
 		
+		buttonChoice(name: number, button: button)
+		
 		button.snp.makeConstraints { make in
 			make.height.equalTo(90)
 			make.width.equalTo(90)
 			make.left.equalToSuperview().inset(valueLeft)
 			make.top.equalTo(valueTop)
 		}
-		
 	}
 	
-	func buttomOperation (valueOpertion:String,valueLeft: Int, valueTop:Int){
-		
-		let button = UIButton()
-		button.setTitle(valueOpertion, for: .normal)
-		button.backgroundColor = UIColor.orange
-		button.addTarget(self, action: #selector(buttonActionOperation), for: .touchUpInside)
-
-		self.view.addSubview(button)
-		
-		button.snp.makeConstraints { make in
-			make.height.equalTo(90)
-			make.width.equalTo(90)
-			make.left.equalToSuperview().inset(valueLeft)
-			make.top.equalTo(valueTop)
+	func buttonChoice(name:String, button:UIButton){
+		switch name {
+		case "/","X","+","-":
+			button.addTarget(self, action: #selector(buttonActionOperation), for: .touchUpInside)
+		case "=":
+			button.addTarget(self, action: #selector(buttonActionEqual), for: .touchUpInside)
+		case "C":
+			button.addTarget(self, action: #selector(buttonReset), for: .touchUpInside)
+		case ".":
+			button.addTarget(self, action: #selector(buttonActionPoint), for: .touchUpInside)
+		default:
+			button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
 		}
 	}
-	func buttomReset (){
-		
-		let button = UIButton()
-		button.setTitle("C", for: .normal)
-		button.backgroundColor = UIColor.orange
-		button.addTarget(self, action: #selector(buttonReset), for: .touchUpInside)
-		
-		self.view.addSubview(button)
-		
-		button.snp.makeConstraints { make in
-			make.height.equalTo(90)
-			make.width.equalTo(90)
-			make.left.equalToSuperview().inset(295)
-			make.top.equalTo(595)
-		}
-	}
+	// MARK:- Реализация логики кнопок
 	@objc func buttonReset(sender: UIButton!){
 		label.text = "0"
 		firstNumber = 0
 		secondNumber = 0
 		ten = 10.0
 		checkPoint = false
-		
-	}
-	func buttomEqually (){
-		
-		let button = UIButton()
-		button.setTitle("=", for: .normal)
-		button.backgroundColor = UIColor.orange
-		button.addTarget(self, action: #selector(buttonActionEqual), for: .touchUpInside)
-		self.view.addSubview(button)
-		
-		button.snp.makeConstraints { make in
-			make.height.equalTo(90)
-			make.width.equalTo(90)
-			make.left.equalToSuperview().inset(295)
-			make.top.equalTo(495)
-		}
 	}
 	
-	func buttomPoint(){
-		
-		let button = UIButton()
-		button.setTitle(".", for: .normal)
-		button.backgroundColor = UIColor.orange
-		button.addTarget(self, action: #selector(buttonActionPoint), for: .touchUpInside)
-
-		self.view.addSubview(button)
-		
-		button.snp.makeConstraints { make in
-			make.height.equalTo(90)
-			make.width.equalTo(90)
-			make.left.equalToSuperview().inset(105)
-			make.top.equalTo(495)
-		}
-	}
 	@objc func buttonActionPoint(sender: UIButton!) {
-		
 		guard let numlabel = label.text else {  return }
 		if let number = Double(numlabel){
-			
 			if number == 0 {
-				//number = 0.0
 				label.text = "0."
 				checkPoint = true
 			} else{
+				label.text?.insert(".", at: label.text!.endIndex)
 				checkPoint = true
 			}
-	
-
-		
+		}
 	}
-	}
-	
 	
 	@objc func buttonAction(sender: UIButton!) {
 		let number = sender.currentTitle!
-		
-		
-		
 		if let numberDouble = Double(number) {
-			
-			if  check {
+			if  checkFirstnumAndSecondnum {
 				if checkPoint {
 					firstNumber += numberDouble/ten
 					ten *= 10
@@ -208,18 +140,15 @@ class ViewController: UIViewController {
 		
 	}
 	
-	
 	@objc func buttonActionOperation(sender: UIButton!) {
 		operation = sender.currentTitle!
 		label.text = operation
-		check = false
+		checkFirstnumAndSecondnum = false
 		checkPoint = false
 		ten = 10.0
-		
 	}
 	
 	@objc func buttonActionEqual(sender: UIButton!) {
-		
 		switch operation {
 		case "+":
 			label.text = forTrailingZero(temp: firstNumber + secondNumber)
@@ -230,9 +159,16 @@ class ViewController: UIViewController {
 		case "-":
 			label.text = forTrailingZero(temp: firstNumber - secondNumber)
 		default:
-			print("er")
+			print("error")
 		}
-		check = true
+		checkFirstnumAndSecondnum = true
 	}
 }
 
+// MARK:- Корректировка вывода Double
+extension ViewController {
+	func forTrailingZero(temp: Double) -> String {
+		let tempVar = String(format: "%g", temp)
+		return tempVar
+	}
+}
